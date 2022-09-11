adb2529e85add504e09db237e14ea7a9647c858c
Hadbanj <adebanjoadekunle56@gmail.com>
2022-09-11 12:07:26 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 
 /**
 * main - print the string in the put function
@@ -7,7 +8,7 @@
 * Return: 0
 */
 int main(void)
-(
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
