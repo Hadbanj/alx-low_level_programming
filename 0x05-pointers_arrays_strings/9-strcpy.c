4d06430675b60a5b7a4044e314b204a1c58f7197
Hadbanj <adebanjoadekunle56@gmail.com>
2022-09-20 08:32:15 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,10 @@
-#include "main.h"
+include "main.h"
 
 /**
 * _strcpy - copy paste string
 * @dest: destination
 * @src: source
-* 
+*
 * Return: destination
 */
 
@@ -18,7 +18,6 @@ char *_strcpy(char *dest, char *src)
 		inc++;
 	}
 	*(dest + inc) = '\0';
-
 
 	return (dest);
 }
